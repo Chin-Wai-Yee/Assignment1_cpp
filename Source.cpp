d7e9b3d4ab4477b960bbf6d628d997521bf697f0
cwy <2103370@1utar.my>
2023-02-20 09:07:00 +0800

    modified according to the pseudocode

--- Source.cpp
+++ Source.cpp
@@ -9,27 +9,20 @@ int main(void) {
 	double a3, a2, a1, a0, b2, b1, b0, delta, x1, x2, y1, y2, r, s, m, t;
 	int num_solution;
 
-	bool proceed = false;
-
-	while (!proceed) {
-		cout << "Cubic Equation calculator" << endl;
-		cout << "Geral cubic equation form: ax^3 + bx^2 +cx +d = 0" << endl;
-		cout << "Please enter the value of a :";
-		cin >> a3;
-		cout << "Please enter the value of b :";
-		cin >> a2;
-		cout << "Please enter the value of c :";
-		cin >> a1;
-		cout << "Please enter the value of d :";
-		cin >> a0;
-		if (a3 != 0) {
-			proceed = true;
-		}
-		else {
-			cout << "Invalid input!" << endl;
-			system("pause");
-			system("cls");
-		}
+	cout << "Cubic Equation calculator" << endl;
+	cout << "Geral cubic equation form: ax^3 + bx^2 +cx +d = 0" << endl;
+	cout << "Please enter the value of a :";
+	cin >> a3;
+	cout << "Please enter the value of b :";
+	cin >> a2;
+	cout << "Please enter the value of c :";
+	cin >> a1;
+	cout << "Please enter the value of d :";
+	cin >> a0;
+	if (a3 <= 0) {
+		cout << "Invalid input!" << endl;
+		system("pause");
+		return 1;
 	}
 
 
@@ -87,7 +80,7 @@ int main(void) {
 			y_m = a3 * pow(m, 3) + a2 * pow(m, 2) + a1 * m + a0;
 			if (y_m == 0 || abs(s - r) < 0.000001) {
 				t = m;
-				cout << "The solution is :" << t << endl;
+				cout << "The solution is :" << fixed << setprecision(6) << t << endl;
 				stop = true;
 			}
 			else if (y_m > 0) {
